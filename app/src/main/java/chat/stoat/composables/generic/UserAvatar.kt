8c0e19f077f0ceb8acafc6ea16bd1f9a1f526100
tonij7853-dotcom <tonij7853@gmail.com>
2026-09-25 00:24:24 +0700

    Fix avatar decoration build import

--- app/src/main/java/chat/stoat/composables/generic/UserAvatar.kt
+++ app/src/main/java/chat/stoat/composables/generic/UserAvatar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
